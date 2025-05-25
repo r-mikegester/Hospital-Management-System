6ee19061319ac9406f3f128c25b9f19814edd385
Mike Gester <mikegester.sabuga023@gmail.com>
2025-05-26 05:08:48 +0800

    Update head.php

--- components/head.php
+++ components/head.php
@@ -5,8 +5,9 @@
     <title>Admin Dashboard</title>
     <!-- TailwindCSS and DaisyUI -->
      <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.2/dist/tailwind.min.css" rel="stylesheet">
-
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <script>
         tailwind.config = {
             plugins: [require('daisyui')],
